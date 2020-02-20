322355095ed6b806bbabe71c7c7632ed0383beea
Swagat Kumar Swain <ssswagatss@gmail.com>
2020-02-20 15:39:53 +0530

    ADDS CONTROLLER

---
+++ Demo2Mvc/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using Demo2Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo2Mvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var data = new DemoModel() { Message= "Hello World from Index page DATA 1" ,EmpId=22};
+            var data2 = new DemoModel() { Message= "Hello World from Index page DATA 2" ,EmpId=23};
+
+            var bar = new List<DemoModel>();
+            bar.Add(data);
+            bar.Add(data2);
+
+            ViewBag.Data = bar;
+
+            return View("~/Views/Employee/MyIndex.cshtml");
+        }
+        public ActionResult Contact()
+        {
+            var data = new DemoModel() { Message = "Hello World from Contact page", EmpId = 22 };
+            return View("~/Views/Employee/MyIndex.cshtml",data);
+        }
+    }
+}
---
+++ Demo2Mvc/Models/DemoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo2Mvc.Models
+{
+    public class DemoModel
+    {
+        public string Message { get; set; }
+        public int  EmpId{ get; set; }
+    }
+}
