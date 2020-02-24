d613d94ffaca5eebc2547e0f214fc70ef4cfe199
Swagat Kumar Swain <ssswagatss@gmail.com>
2020-02-24 14:33:08 +0530

    routing

---
+++ Demo2Mvc/Controllers/DemoController.cs
@@ -0,0 +1,22 @@
+﻿using Demo2Mvc.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo2Mvc.Controllers
+{
+    public class DemoController : Controller
+    {
+        public ActionResult Abc()
+        {
+            return View();
+        }
+        public ActionResult Xyz(DemoVm model)
+        {
+           
+            return View(model);
+        }
+    }
+}
---
+++ Demo2Mvc/Models/ViewModels/DemoVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo2Mvc.Models.ViewModels
+{
+    public class DemoVm
+    {
+        public string A { get; set; }
+        public string B { get; set; }
+    }
+}
